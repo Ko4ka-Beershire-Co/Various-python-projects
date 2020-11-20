d391d263c5655c30e189429efaab604351a12f83
Alexander Kuznetsov <alex@beershire.ru>
2020-11-20 13:26:39 +0300

    Update sorter.py

--- sorter.py
+++ sorter.py
@@ -2,19 +2,36 @@
 import os.path
 import shutil
 
-wav_dir = 'C://Users/Alex/Desktop/Folder'
+wav_dir = 'C://Users/Alex/Desktop/Folder3'
 ass_dir = 'C://Users/Alex/Desktop/Folder2'
 
 ass_dir_new = 'C://Users/Alex/Desktop/Folder2/New'
+start = 0
+stop = 3
+while start < stop:
+# Counter for 00-15
+    if start < 10:
+        sub_dir = '0' + str(start)
+    elif start >= 10:
+        sub_dir = str(start)
 
-for filename in os.listdir(ass_dir):
-    if os.path.isfile(str(ass_dir) + '/' + str(filename)):
-        os.remove(str(ass_dir) + '/' + str(filename)[:-4] + '.wav')
+# Move .wav to target
+    source_dir = str(wav_dir) + '/' + str(sub_dir)
+    wav = os.listdir(source_dir)
+    for filename in wav:
+        shutil.move(os.path.join(source_dir, filename), ass_dir)
 
-        shutil.move(str(ass_dir) + "/" + str(filename), str(ass_dir_new) + "/" + str(filename))
-        print(str(filename)[:-4] + '.wav')
+# Match, move
+    for filename in os.listdir(ass_dir):
+        if os.path.isfile(str(ass_dir) + '/' + str(filename)[:-4] + '.wav'):
+            os.remove(str(ass_dir) + '/' + str(filename)[:-4] + '.wav')
+            new_dir = str(ass_dir_new) + '/' + str(sub_dir)
+            shutil.move(str(ass_dir) + "/" + str(filename), str(new_dir) + '/' + str(filename))
+            print(str(filename)[:-4] + '.wav')
 
+# Remove
+    for filename in os.listdir(ass_dir_new):
+        if filename.endswith(".wav"):
+            os.remove(os.path.join(ass_dir_new, filename))
 
-for filename in os.listdir(ass_dir_new):
-    if filename.endswith(".wav"):
-        os.remove(os.path.join(ass_dir_new, filename))
+    start = start + 1
