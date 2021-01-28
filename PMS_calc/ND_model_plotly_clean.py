9f1cd96f44996cd4583c9eb48f9fcbfa95e31fc1
Alexander Kuznetsov <alex@beershire.ru>
2021-01-28 15:01:08 +0300

    Update ND_model_plotly_clean.py

--- PMS_calc/ND_model_plotly_clean.py
+++ PMS_calc/ND_model_plotly_clean.py
@@ -6,23 +6,31 @@ import plotly.graph_objects as go
 import plotly.express as plt
 import scipy.stats as stats
 
+
+# Dist as class
+class Process:
+    def __init__(self, mu, sigma, n, lower, upper):
+        self.mu = mu
+        self.sigma = sigma
+        self.n = n
+        self.lower = lower
+        self.upper = upper
+
+
 # Cycle Distribution
-mu, sigma, n = 28, 3, 100000
-lower, upper = 21, 35
+Cycle = Process(28, 3, 100000, 21, 35)
+
 
 # Duration Distribution
-d_mu, d_sigma, d_n = 5, 1, 100000
-d_lower, d_upper = 2, 7
+duration = Process(5, 1, 100000, 2, 7)
+
+
+def scatter_plot(X, Y):  # if needed
+    fig = plt.scatter(x=X, y=Y)
+    fig.show()
 
 
 def create_array_cont():
-    # Cycle Distribution
-    mu, sigma, n = 28, 3, 100000
-    lower, upper = 21, 35
-
-    # Duration Distribution
-    d_mu, d_sigma, d_n = 5, 1, 100000
-    d_lower, d_upper = 2, 7
 
     def normal(x, mu, sigma):
         return (2. * np.pi * sigma ** 2.) ** -.5 * np.exp(-.5 * (x - mu) ** 2. / sigma ** 2.)
@@ -30,22 +38,21 @@ def create_array_cont():
     # Cycle Array
 
     X = stats.truncnorm.rvs(
-        (lower - mu) / sigma, (upper - mu) / sigma, loc=mu, scale=sigma, size=n)
-    Y = normal(X, mu, sigma)
+        (Cycle.lower - Cycle.mu) / Cycle.sigma, (Cycle.upper - Cycle.mu) / Cycle.sigma, loc=Cycle.mu, scale=Cycle.sigma,
+        size=Cycle.n)
+    Y = normal(X, Cycle.mu, Cycle.sigma)
 
     # Plot: ND allied to Cycle will assume conventional form, even when truncated
-    # fig = plt.scatter(x=X, y=Y)
-    # fig.show()
+
 
     # Duration Array
 
     d_X = stats.truncnorm.rvs(
-        (d_lower - d_mu) / d_sigma, (d_upper - d_mu) / d_sigma, loc=d_mu, scale=d_sigma, size=d_n)
-    d_Y = normal(d_X, d_mu, d_sigma)
+        (duration.lower - duration.mu) / duration.sigma, (duration.upper - duration.mu) / duration.sigma,
+        loc=duration.mu, scale=duration.sigma, size=duration.n)
+    d_Y = normal(d_X, duration.mu, duration.sigma)
 
     # Plot: ND allied to Duration will not assume conventional form as MU has a +1 displacement
-    # fig = plt.scatter(x=d_X, y=d_Y)
-    # fig.show()
 
     Z = Y * d_Y
 
@@ -64,10 +71,10 @@ def plot_cont(array):
     # Set axis range
     fig.update_layout(
         scene=dict(
-            xaxis=dict(nticks=1, range=[lower, upper], ticks='outside',
-                       tick0=lower, dtick=1, tickwidth=0.01),
-            yaxis=dict(nticks=1, range=[d_lower, d_upper], ticks='outside',
-                       tick0=d_lower, dtick=1, tickwidth=0.01),
+            xaxis=dict(nticks=1, range=[Cycle.lower, Cycle.upper], ticks='outside',
+                       tick0=Cycle.lower, dtick=1, tickwidth=0.01),
+            yaxis=dict(nticks=1, range=[duration.lower, duration.upper], ticks='outside',
+                       tick0=duration.lower, dtick=1, tickwidth=0.01),
             zaxis=dict(nticks=1, range=[0, 0.07], ticks='outside',
                        tick0=0, dtick=0.01, tickwidth=0.01), ), ),
 
