7f4db9771c53f8a6afe08bbd71d2e3c12e596622
Alexander Kuznetsov <alex@beershire.ru>
2021-03-19 22:03:17 +0300

    Update advanced_NPL.py

--- Bussiness Diving/advanced_NPL.py
+++ Bussiness Diving/advanced_NPL.py
@@ -5,25 +5,25 @@ from collections import Counter
 import nltk
 
 nltk.download('stopwords')
-#from nltk.corpus import stopwords
+# from nltk.corpus import stopwords
 from nltk.stem import SnowballStemmer
 
 # Важные переменные
 stopwords = ['ourselves', 'hers', 'between', 'yourself', 'but', 'again', 'there', 'about', 'once', 'during',
-                   'out', 'very', 'having', 'with', 'they', 'own', 'an', 'be', 'some', 'for', 'do', 'its', 'yours',
-                   'such', 'into', 'of',
-                   'most', 'itself', 'other', 'off', 'is', 's', 'am', 'or', 'who', 'as', 'from', 'him', 'each', 'the',
-                   'themselves', 'until', 'below', 'are', 'we', 'these', 'your', 'his', 'through', 'don', 'nor', 'me',
-                   'were', 'her', 'more', 'himself', 'this', 'down', 'should', 'our', 'their', 'while', 'above', 'both',
-                   'up', 'to', 'ours', 'had', 'she', 'all', 'no', 'when', 'at', 'any', 'before', 'them', 'same', 'and',
-                   'been', 'have', 'in', 'will', 'on', 'does', 'yourselves', 'then', 'that', 'because', 'what', 'over',
-                   'why', 'so', 'can', 'did', 'not', 'now', 'under', 'he', 'you', 'herself', 'has', 'just', 'where',
-                   'too', 'only', 'myself', 'which', 'those', 'i', 'after', 'few', 'whom', 'it', 'being', 'if',
-                   'theirs',
-                   'my', 'against', 'a', 'by', 'doing', 'it', 'how', 'further', 'was', 'here', 'than','"had']
+             'out', 'very', 'having', 'with', 'they', 'own', 'an', 'be', 'some', 'for', 'do', 'its', 'yours',
+             'such', 'into', 'of',
+             'most', 'itself', 'other', 'off', 'is', 's', 'am', 'or', 'who', 'as', 'from', 'him', 'each', 'the',
+             'themselves', 'until', 'below', 'are', 'we', 'these', 'your', 'his', 'through', 'don', 'nor', 'me',
+             'were', 'her', 'more', 'himself', 'this', 'down', 'should', 'our', 'their', 'while', 'above', 'both',
+             'up', 'to', 'ours', 'had', 'she', 'all', 'no', 'when', 'at', 'any', 'before', 'them', 'same', 'and',
+             'been', 'have', 'in', 'will', 'on', 'does', 'yourselves', 'then', 'that', 'because', 'what', 'over',
+             'why', 'so', 'can', 'did', 'not', 'now', 'under', 'he', 'you', 'herself', 'has', 'just', 'where',
+             'too', 'only', 'myself', 'which', 'those', 'i', 'after', 'few', 'whom', 'it', 'being', 'if',
+             'theirs',
+             'my', 'against', 'a', 'by', 'doing', 'it', 'how', 'further', 'was', 'here', 'than', '"had']
 snowball = SnowballStemmer(language="english")
 json_file = 'buffer.json'
-TOP = 5  # кол-во токенов для определения семантики
+TOP = 10  # кол-во токенов для определения семантики
 
 with open(json_file, encoding='utf-8') as f:  # Допустим, открыли JSON как словарь
     data = f.read()
@@ -34,7 +34,6 @@ with open(json_file, encoding='utf-8') as f:  # Допустим, открыли
 
     for line in comment_list:  # для каждого коммента вывести топ Х слов
         split_it = line.split()  # превращаю строку в массив [слово, слово, ...]
-
         j = len(split_it)
         i = 0
         while i < j:
@@ -43,19 +42,18 @@ with open(json_file, encoding='utf-8') as f:  # Допустим, открыли
                 split_it.pop(i)
                 j -= 1
             else:
+                split_it[i] = snowball.stem(z)  # теперь стемирование прям в цикле (урезаю слово до сволоформы)
                 i += 1
-
-        for i in split_it:  # стемирую каждое слово: слово -> словоформа
-            for j in i:
-                snowball.stem(i)
+        # print(split_it)
         z = Counter(split_it).most_common(TOP)  # вывожу массив [(словоформа1, разы), ..., (словоформаTOP, разы)]
-        step = 0
-        for i in z:  # step - обходит tuple_index
-            step += 1
-            count = 0
-            for j in i:
-                count +=1
-                if count %2 == 0:  # для каждого второго эл-та в j т.е. разов
-                    j = j * int(value_list[step-1])  # домножу на кол-во лайков - лайкнул - согласен
+        #print(z)
+        z_list = [list(elem) for elem in z]
 
-        print(z)  # TOP словоформ для каждого коммента"""
+        step = 0
+        for n in z_list: # использую лайки в качестве подтверждения правдивости интента
+            step += 1
+            for m in n:
+                #print(n[1])
+                n[1] = n[1] + int(value_list[step-1])
+        print(z_list)
+        
