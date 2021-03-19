e9feb6dbc8788a705e820d12b17ad6499909955e
Alexander Kuznetsov <alex@beershire.ru>
2021-03-19 18:45:25 +0300

    Create utils.py

---
+++ Bussiness Diving/utils.py
@@ -0,0 +1,44 @@
+import nltk
+
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem import SnowballStemmer
+
+stopwords = stopwords.words("english")
+
+h = ['Had', 'the', 'game', 'before', 'and', 'loved', 'it', 'but', 'after', 'the', 'last', 'update', 'the', 'game',
+     'will', 'not', 'load.', 'It', 'starts', 'and', 'at', 'the', 'Niantic', 'screen', 'it', 'force', 'closes', 'and',
+     'I', 'had', 'get', 'sent', 'back', 'to', 'my', 'home', 'screen.', 'Samsung', 'note', '4",']
+
+words_to_ignore = ['ourselves', 'hers', 'between', 'yourself', 'but', 'again', 'there', 'about', 'once', 'during',
+                   'out', 'very', 'having', 'with', 'they', 'own', 'an', 'be', 'some', 'for', 'do', 'its', 'yours',
+                   'such', 'into', 'of',
+                   'most', 'itself', 'other', 'off', 'is', 's', 'am', 'or', 'who', 'as', 'from', 'him', 'each', 'the',
+                   'themselves', 'until', 'below', 'are', 'we', 'these', 'your', 'his', 'through', 'don', 'nor', 'me',
+                   'were', 'her', 'more', 'himself', 'this', 'down', 'should', 'our', 'their', 'while', 'above', 'both',
+                   'up', 'to', 'ours', 'had', 'she', 'all', 'no', 'when', 'at', 'any', 'before', 'them', 'same', 'and',
+                   'been', 'have', 'in', 'will', 'on', 'does', 'yourselves', 'then', 'that', 'because', 'what', 'over',
+                   'why', 'so', 'can', 'did', 'not', 'now', 'under', 'he', 'you', 'herself', 'has', 'just', 'where',
+                   'too', 'only', 'myself', 'which', 'those', 'i', 'after', 'few', 'whom', 'it', 'being', 'if',
+                   'theirs',
+                   'my', 'against', 'a', 'by', 'doing', 'it', 'how', 'further', 'was', 'here', 'than']
+
+j = len(h)
+i = 0
+while i < j:
+    z = h[i]
+    if z.lower() in words_to_ignore:
+        print(z)
+        h.pop(i)
+        j -= 1
+    else:
+        i += 1
+
+print(h)
+"""counter = 0
+for i in h:
+    counter += 1
+    if i.lower() in words_to_ignore:
+        print(i)
+        h.append()
+print(h)"""
