1b5be8f3cf18c2526e32f3a2a808de1f8ecaec9f
Alexander Kuznetsov <alex@beershire.ru>
2021-03-19 18:51:41 +0300

    Update advanced_NPL.py

--- Bussiness Diving/advanced_NPL.py
+++ Bussiness Diving/advanced_NPL.py
@@ -5,11 +5,22 @@ from collections import Counter
 import nltk
 
 nltk.download('stopwords')
-from nltk.corpus import stopwords
+#from nltk.corpus import stopwords
 from nltk.stem import SnowballStemmer
 
 # Важные переменные
-stopwords.words("english")
+stopwords = ['ourselves', 'hers', 'between', 'yourself', 'but', 'again', 'there', 'about', 'once', 'during',
+                   'out', 'very', 'having', 'with', 'they', 'own', 'an', 'be', 'some', 'for', 'do', 'its', 'yours',
+                   'such', 'into', 'of',
+                   'most', 'itself', 'other', 'off', 'is', 's', 'am', 'or', 'who', 'as', 'from', 'him', 'each', 'the',
+                   'themselves', 'until', 'below', 'are', 'we', 'these', 'your', 'his', 'through', 'don', 'nor', 'me',
+                   'were', 'her', 'more', 'himself', 'this', 'down', 'should', 'our', 'their', 'while', 'above', 'both',
+                   'up', 'to', 'ours', 'had', 'she', 'all', 'no', 'when', 'at', 'any', 'before', 'them', 'same', 'and',
+                   'been', 'have', 'in', 'will', 'on', 'does', 'yourselves', 'then', 'that', 'because', 'what', 'over',
+                   'why', 'so', 'can', 'did', 'not', 'now', 'under', 'he', 'you', 'herself', 'has', 'just', 'where',
+                   'too', 'only', 'myself', 'which', 'those', 'i', 'after', 'few', 'whom', 'it', 'being', 'if',
+                   'theirs',
+                   'my', 'against', 'a', 'by', 'doing', 'it', 'how', 'further', 'was', 'here', 'than','"had']
 snowball = SnowballStemmer(language="english")
 json_file = 'buffer.json'
 TOP = 5  # кол-во токенов для определения семантики
@@ -23,12 +34,23 @@ with open(json_file, encoding='utf-8') as f:  # Допустим, открыли
 
     for line in comment_list:  # для каждого коммента вывести топ Х слов
         split_it = line.split()  # превращаю строку в массив [слово, слово, ...]
+
+        j = len(split_it)
+        i = 0
+        while i < j:
+            z = split_it[i]
+            if z.lower() in stopwords:
+                split_it.pop(i)
+                j -= 1
+            else:
+                i += 1
+
         for i in split_it:  # стемирую каждое слово: слово -> словоформа
             for j in i:
                 snowball.stem(i)
-        z = Counter(split_it).most_common(TOP)  # вывожу массив [(словоформа1, разы), ..., (словоформаTOP, разы)] 
+        z = Counter(split_it).most_common(TOP)  # вывожу массив [(словоформа1, разы), ..., (словоформаTOP, разы)]
         step = 0
-        for i in z:  # step - обходит tuple_index 
+        for i in z:  # step - обходит tuple_index
             step += 1
             count = 0
             for j in i:
@@ -36,4 +58,4 @@ with open(json_file, encoding='utf-8') as f:  # Допустим, открыли
                 if count %2 == 0:  # для каждого второго эл-та в j т.е. разов
                     j = j * int(value_list[step-1])  # домножу на кол-во лайков - лайкнул - согласен
 
-        print(z)  # TOP словоформ для каждого коммента
+        print(z)  # TOP словоформ для каждого коммента"""
