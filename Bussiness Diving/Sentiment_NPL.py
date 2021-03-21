674c1f364b8caf93a60f3d72a51d23bff0f1a696
Alexander Kuznetsov <alex@beershire.ru>
2021-03-21 22:34:21 +0300

    Update Sentiment_NPL.py

--- Bussiness Diving/Sentiment_NPL.py
+++ Bussiness Diving/Sentiment_NPL.py
@@ -9,24 +9,36 @@ nltk.download('stopwords')
 from nltk.stem import SnowballStemmer
 
 # Важные переменные
-stopwords = ['ourselves', 'hers', 'between', 'yourself', 'but', 'again', 'there', 'about', 'once', 'during',
-             'out', 'very', 'having', 'with', 'they', 'own', 'an', 'be', 'some', 'for', 'do', 'its', 'yours',
-             'such', 'into', 'of',
-             'most', 'itself', 'other', 'off', 'is', 's', 'am', 'or', 'who', 'as', 'from', 'him', 'each', 'the',
-             'themselves', 'until', 'below', 'are', 'we', 'these', 'your', 'his', 'through', 'don', 'nor', 'me',
-             'were', 'her', 'more', 'himself', 'this', 'down', 'should', 'our', 'their', 'while', 'above', 'both',
-             'up', 'to', 'ours', 'had', 'she', 'all', 'no', 'when', 'at', 'any', 'before', 'them', 'same', 'and',
-             'been', 'have', 'in', 'will', 'on', 'does', 'yourselves', 'then', 'that', 'because', 'what', 'over',
-             'why', 'so', 'can', 'did', 'not', 'now', 'under', 'he', 'you', 'herself', 'has', 'just', 'where',
-             'too', 'only', 'myself', 'which', 'those', 'i', 'after', 'few', 'whom', 'it', 'being', 'if',
-             'theirs',
-             'my', 'against', 'a', 'by', 'doing', 'it', 'how', 'further', 'was', 'here', 'than', '"had']
-snowball = SnowballStemmer(language="english")
+stopwords_eng = ['ourselves', 'hers', 'between', 'yourself', 'but', 'again', 'there', 'about', 'once', 'during',
+                 'out', 'very', 'having', 'with', 'they', 'own', 'an', 'be', 'some', 'for', 'do', 'its', 'yours',
+                 'such', 'into', 'of',
+                 'most', 'itself', 'other', 'off', 'is', 's', 'am', 'or', 'who', 'as', 'from', 'him', 'each', 'the',
+                 'themselves', 'until', 'below', 'are', 'we', 'these', 'your', 'his', 'through', 'don', 'nor', 'me',
+                 'were', 'her', 'more', 'himself', 'this', 'down', 'should', 'our', 'their', 'while', 'above', 'both',
+                 'up', 'to', 'ours', 'had', 'she', 'all', 'no', 'when', 'at', 'any', 'before', 'them', 'same', 'and',
+                 'been', 'have', 'in', 'will', 'on', 'does', 'yourselves', 'then', 'that', 'because', 'what', 'over',
+                 'why', 'so', 'can', 'did', 'not', 'now', 'under', 'he', 'you', 'herself', 'has', 'just', 'where',
+                 'too', 'only', 'myself', 'which', 'those', 'i', 'after', 'few', 'whom', 'it', 'being', 'if',
+                 'theirs',
+                 'my', 'against', 'a', 'by', 'doing', 'it', 'how', 'further', 'was', 'here', 'than', '"had']
+
+stopwords_rus = ['и', 'в', 'во', 'не', 'что', 'он', 'на', 'я', 'с', 'со', 'как', 'а', 'то', 'все', 'чтоб', 'без',
+                 'будто', 'впрочем', 'хорошо', 'свою', 'этой', 'перед', 'иногда', 'лучше', 'чуть', 'том', 'нельзя',
+                 'такой', 'им', 'более', 'всегда', 'конечно', 'всю', 'между', 'у', 'это', 'все', 'его', 'ее', 'от',
+                 'бы', 'как', 'для', 'еще', 'но', 'так', 'вы', 'вот', 'к', 'уж', 'мне', 'до', 'еще', 'был', 'при', 'по',
+                 'ни']
+
+snowball = SnowballStemmer(language="russian")
 json_file = 'buffer.json'
 TOP = 10  # кол-во токенов для определения семантики
 
 
-def create_corpus():
+def create_corpus(lang):
+    if lang == 'rus':
+        stopwords = stopwords_rus
+    else:
+        stopwords = stopwords_eng
+
     with open('corpus.txt', 'w+', encoding='utf-8') as c:
         with open(json_file, encoding='utf-8') as f:  # Допустим, открыли JSON как словарь
             data = f.read()
@@ -38,7 +50,9 @@ def create_corpus():
             step = 0
             for line in comment_list:  # для каждого коммента вывести топ Х слов
                 line = re.sub(r"\'|\"|\\\\", '', line, 0, re.MULTILINE)
-                line = re.sub(r"[,.:]", "", line, 0)
+                line = re.sub(r"[,.:!()]", "", line, 0)
+                if lang == 'rus':
+                    line = re.sub(r' не ', ' не', line, 0)  # if russian
                 step += 1
                 split_it = line.split()  # превращаю строку в массив [слово, слово, ...]
                 j = len(split_it)
@@ -80,5 +94,5 @@ def corpus_counter(corpus_file, top):
 
 
 # Fire it up!
-create_corpus()
+create_corpus('rus')
 corpus_counter('corpus.txt', 100)
