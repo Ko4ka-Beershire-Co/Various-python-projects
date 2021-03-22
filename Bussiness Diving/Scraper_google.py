109531357990a8cec0a1fd8d5c610f93f7d7f99f
Alexander Kuznetsov <alex@beershire.ru>
2021-03-22 15:19:01 +0300

    Update Scraper_google.py

--- Bussiness Diving/Scraper_google.py
+++ Bussiness Diving/Scraper_google.py
@@ -1,24 +1,34 @@
-from google_play_scraper import Sort, reviews_all
 import json
 
+# Application ru-com operators
 
-def scarp_and_write(buffer_name):
+okko = 'ru.more.play'
+ivi = 'ru.ivi.client'
+kinopoisk = 'ru.kinopoisk'
+tnt_premier = 'gpm.tnt_premier'
+start = 'ru.start.androidmobile'
+netflix = 'com.netflix.mediaclient'
+megogo = 'com.megogo.application'
+wink = 'ru.rt.video.app.mobile'
+amediateka = 'com.amdteka'
+
+
+def scarp_and_write(buffer_name, application):
     result = reviews_all(
-        'com.nianticlabs.pokemongo',
-        sleep_milliseconds=0,  
-        lang='en',  # Язык %LL%
-        country='us',  # Страна %СС%
-        sort=Sort.MOST_RELEVANT,  # Сортировка из Google Play
-        filter_score_with=1  # Выводить только с %I% оценкой
+        application,
+        sleep_milliseconds=0,  # defaults to 0
+        lang='ru',  # defaults to 'en'
+        country='ru',  # defaults to 'us'
+        sort=Sort.MOST_RELEVANT,  # defaults to Sort.MOST_RELEVANT
+        filter_score_with=1  # defaults to None(means all score)
     )
     with open(buffer_name, 'w', encoding='utf-8') as f:
+        json.dump(result, f, ensure_ascii=False, indent=4, default=str)
 
-        json.dump(result, f, ensure_ascii=False, indent=4, default=str)  # JSON сериалайзабилити мне теперь сниться будет
 
-
-def clear_buffer(buffer_name):  # Если он есть
+def clear_buffer(buffer_name):
     open(buffer_name, 'w').close()
 
 
 clear_buffer('buffer.json')
-scarp_and_write('buffer.json')
+scarp_and_write('buffer.json', okko)
