74ca77c2444cffcb27eece0f9074545c3333b2e4
Alexander Kuznetsov <alex@beershire.ru>
2021-03-29 15:12:48 +0300

    Update Error_list.py

--- Error_finder/Error_list.py
+++ Error_finder/Error_list.py
@@ -1,6 +1,15 @@
 import re
+import os
 
-with open ('import 275 - source data errors.json', 'r+', encoding='utf-8',) as f:
-    body = f.read()
-    error_list = re.findall(r'\"filename\": \"(.*)\"', body, re.MULTILINE)
-    print(set(error_list))
+error_file_path = "C://Users/Alex/Desktop/Python/Markup/Error_finder/Errors"
+# script_path = "C://Users/Alex/Desktop/Python/Markup/Error_finder"
+file_names = []
+
+for filename in os.listdir(error_file_path):
+
+    with open(error_file_path + '/' + filename, 'r+', encoding='utf-8', ) as f:
+        body = f.read()
+        error_list = re.findall(r'\"filename\": \"(.*)\"', body, re.MULTILINE)
+        file_names.append(set(error_list))
+
+print(file_names)
