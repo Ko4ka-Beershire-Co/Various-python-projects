6d09a19a027b309b54a5e08435280242a82169d6
Alexander Kuznetsov <alex@beershire.ru>
2021-04-22 13:18:02 +0300

    Create get_html_tree.py

---
+++ get_html_tree.py
@@ -0,0 +1,36 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def parser():
+    URL = 'https://www.speechpro.ru/'
+    HEADERS = {'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                             'Chrome/84.0.4147.105 Safari/537.36', 'accept': '*/*'}  # Real params
+
+    # Request to send
+    def get_html(url, params=None):
+        r = requests.get(url, headers=HEADERS, params=params)
+        return r
+
+    # Parse html tree and return class containing the price
+    def get_content(html):
+        soup = BeautifulSoup(html, 'html.parser')
+
+        return soup
+
+    # If error check and get full html tree
+    def parse():
+        html = get_html(URL)
+        if html.status_code == 200:
+            j = get_content(html.text)
+        else:
+            print('Чет не работает нихрена...')
+            j = 'Error'
+        return j
+
+    return str(parse())
+
+
+if __name__ == "__main__":
+    with open ('dump.txt', 'w+', encoding='utf-8') as d:
+        d.write(parser())
