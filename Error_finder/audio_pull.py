dcc99f1a3b34fe2f0461aa8de391e44f395da754
Alexander Kuznetsov <alex@beershire.ru>
2021-07-01 14:18:20 +0300

    Update audio_pull.py

--- Error_finder/audio_pull.py
+++ Error_finder/audio_pull.py
@@ -8,6 +8,7 @@ txt = "C://Users/Alex/Desktop/Датасет эмоции/2ГИС"  # .txt names
 # target = "C://Users/Alex/Desktop/Python/Markup/Error_finder/Target" # where do I put the missing files
 # script_path = "C://Users/Alex/Desktop/Python/Markup/Error_finder"
 file_names = []
+stage = 1
 
 for file in os.listdir(txt):
     # Get names
@@ -23,7 +24,7 @@ for file in os.listdir(txt):
 
         for j in i:
             shutil.copy(audio + '/' + j + '.ogg', txt + '/' + file)
-            
-    file_names.clear()
 
-print(file_names)
+    file_names.clear()
+    print('Stage' + '' + stage + 'complete')
+    stage += 1
